a1f32aae0d23c90c446e86aabd152cb7e05cc4b7
Adnan Abbasi <adnan_abbasi@yahoo.com>
2023-06-27 14:03:08 -0400

    End of Utilities Section

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import ip from 'ip';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import HttpStatus from './controller/patient.controller.js';
+import logger from './util/logger.js';
 
 dotenv.config();
 const PORT = process.env.SERVER_PORT || 3000;
@@ -9,6 +11,5 @@ const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
-app.get('/', (req, res) => { res.send({ message: 'UP' }) });
-console.log(process.env);
-app.listen(PORT, () => console.log(`Server running on: ${ip.address()}:${PORT}`));
+app.get('/', (req, res) => res.send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Patient API, v1.0.0 - All Systems Go')));
+app.listen(PORT, () => logger.info(`Server running on: ${ip.address()}:${PORT}`));
