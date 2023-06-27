fe7b65d5f63716886b57708f0a9757ae5d661a5e
Adnan Abbasi <adnan_abbasi@yahoo.com>
2023-06-27 17:44:39 -0400

    End of Routes Section - Created and called routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import express from 'express';
 import ip from 'ip';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import Response from './domain/response.js';
 import HttpStatus from './controller/patient.controller.js';
+import patientRoutes from './route/patient.route.js';
 import logger from './util/logger.js';
 
 dotenv.config();
@@ -11,5 +13,8 @@ const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
+app.use('/patients', patientRoutes);
 app.get('/', (req, res) => res.send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Patient API, v1.0.0 - All Systems Go')));
+app.all('*', (req, res) => res.status(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status)
+    .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, 'Route does not eixst on the server')));
 app.listen(PORT, () => logger.info(`Server running on: ${ip.address()}:${PORT}`));
